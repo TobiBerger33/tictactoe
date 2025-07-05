05601aff088b0da2c5a9797ff0b87015f2681d12
Michael Lucuta <ml@newsrooms.ai>
2025-07-05 13:17:51 +0200

    add win&draw detection

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,25 +9,56 @@ public class Main {
         TicTacToe game = new TicTacToe();
         game.start();
 
-        while(true) {
-            System.out.println("Player: " + game.getCurrentPlayer().getMarker() +
-                                    ", where do you want to place your marker?");
-
-            System.out.print("row: ");
-            int x = scanner.nextInt();
-            x--;
-
-            System.out.print("column: ");
-            int y = scanner.nextInt();
-            y--;
-
-            if(game.getBoard().isCellEmpty(x, y)) {
-                game.getBoard().place(x, y, game.getCurrentPlayer().getMarker());
-                game.getBoard().print();
-                game.switchCurrentPlayer();
-            } else {
-                System.out.println("This cell is occupied! Try again!");
-            }
+        while (game.getGameState() == TicTacToe.GameState.IN_PROGRESS) {
+            playTurn(scanner, game);
         }
+
+        displayGameResult(game);
+    }
+
+    private static void playTurn(Scanner scanner, TicTacToe game) {
+        System.out.println("Player: " + game.getCurrentPlayer().getMarker() +
+                ", where do you want to place your marker?");
+
+        System.out.print("row: ");
+        int x = scanner.nextInt();
+        x--;
+
+        System.out.print("column: ");
+        int y = scanner.nextInt();
+        y--;
+
+        if (game.getBoard().isCellEmpty(x, y)) {
+            game.getBoard().place(x, y, game.getCurrentPlayer().getMarker());
+            game.getBoard().print();
+            game.updateGameState();
+
+            if (game.getGameState() == TicTacToe.GameState.IN_PROGRESS) {
+                game.switchCurrentPlayer();
+            }
+        } else {
+            System.out.println("This cell is occupied! Try again!");
+        }
+    }
+
+    private static void displayGameResult(TicTacToe game) {
+        System.out.println("\n==================");
+        System.out.println("   GAME OVER!");
+        System.out.println("==================");
+
+        switch (game.getGameState()) {
+            case X_WINS:
+                System.out.println("🎉 Player X wins! Congratulations!");
+                break;
+            case O_WINS:
+                System.out.println("🎉 Player O wins! Congratulations!");
+                break;
+            case DRAW:
+                System.out.println("It's a draw! Well played both!");
+                break;
+            default:
+                break;
+        }
+        System.out.println("==================\n");
     }
 }
--- src/test/java/org/example/BoardTest.java
+++ src/test/java/org/example/BoardTest.java
@@ -50,5 +50,69 @@ class BoardTest
         assertThrows(ArrayIndexOutOfBoundsException.class, () -> board.place(3, 3, 'T'));
     }
 
+    @Test
+    void testCheckWin_HorizontalWin()
+    {
+        Board board = new Board();
+        board.place(0, 0, 'X');
+        board.place(0, 1, 'X');
+        board.place(0, 2, 'X');
+        assertTrue(board.checkWin('X'));
+    }
+
+    @Test
+    void testCheckWin_VerticalWin()
+    {
+        Board board = new Board();
+        board.place(0, 0, 'O');
+        board.place(1, 0, 'O');
+        board.place(2, 0, 'O');
+        assertTrue(board.checkWin('O'));
+    }
+
+    @Test
+    void testCheckWin_DiagonalWin()
+    {
+        Board board = new Board();
+        board.place(0, 0, 'X');
+        board.place(1, 1, 'X');
+        board.place(2, 2, 'X');
+        assertTrue(board.checkWin('X'));
+    }
+
+    @Test
+    void testCheckWin_NoWin()
+    {
+        Board board = new Board();
+        board.place(0, 0, 'X');
+        board.place(0, 1, 'O');
+        board.place(0, 2, 'X');
+        assertFalse(board.checkWin('X'));
+        assertFalse(board.checkWin('O'));
+    }
+
+    @Test
+    void testIsFull_EmptyBoard()
+    {
+        Board board = new Board();
+        assertFalse(board.isFull());
+    }
+
+    @Test
+    void testIsFull_FullBoard()
+    {
+        Board board = new Board();
+        board.place(0, 0, 'X');
+        board.place(0, 1, 'O');
+        board.place(0, 2, 'X');
+        board.place(1, 0, 'O');
+        board.place(1, 1, 'X');
+        board.place(1, 2, 'O');
+        board.place(2, 0, 'X');
+        board.place(2, 1, 'O');
+        board.place(2, 2, 'X');
+        assertTrue(board.isFull());
+    }
+
 
 }
